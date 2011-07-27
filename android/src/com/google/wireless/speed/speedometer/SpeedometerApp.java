7311e0124985481bb48cb8431b17ed99d009023a
wenjiezeng <wenjiezeng@google.com>
2011-07-27 15:42:38 -0700

    ready for push

--- android/src/com/google/wireless/speed/speedometer/SpeedometerApp.java
+++ android/src/com/google/wireless/speed/speedometer/SpeedometerApp.java
@@ -5,12 +5,9 @@ package com.google.wireless.speed.speedometer;
 import com.google.wireless.speed.speedometer.MeasurementScheduler.SchedulerBinder;
 import com.google.wireless.speed.speedometer.util.RuntimeUtil;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.app.TabActivity;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.res.Resources;
@@ -33,26 +30,24 @@ import java.security.Security;
 public class SpeedometerApp extends TabActivity {
   
   public static final String TAG = "Speedometer";
-  private final int EXIT_DIALOG_ID = 0;
   
   private MeasurementScheduler scheduler;
-  private AlertDialog exitConfirmationDialog;
   private boolean isBounded = false;  
   /** Defines callbacks for service binding, passed to bindService() */
   private ServiceConnection serviceConn = new ServiceConnection() {
-      @Override
-      public void onServiceConnected(ComponentName className,
-              IBinder service) {
-          // We've bound to LocalService, cast the IBinder and get LocalService instance
-          SchedulerBinder binder = (SchedulerBinder) service;
-          scheduler = binder.getService();
-          isBounded = true;
-      }
+    @Override
+    public void onServiceConnected(ComponentName className, IBinder service) {
+      // We've bound to LocalService, cast the IBinder and get LocalService
+      // instance
+      SchedulerBinder binder = (SchedulerBinder) service;
+      scheduler = binder.getService();
+      isBounded = true;
+    }
 
-      @Override
-      public void onServiceDisconnected(ComponentName arg0) {
-        isBounded = false;
-      }
+    @Override
+    public void onServiceDisconnected(ComponentName arg0) {
+      isBounded = false;
+    }
   };
   
   /** Returns the scheduler singleton instance */
@@ -88,16 +83,16 @@ public class SpeedometerApp extends TabActivity {
    *  menu pops up */
   @Override
   public boolean onPrepareOptionsMenu (Menu menu) {
-    setPauseIconBasedOnSchedulerState(menu.findItem(R.id.menuPause));
+    setPauseIconBasedOnSchedulerState(menu.findItem(R.id.menuPauseResume));
     return true;
   }
   
   /** React to menu item selections */
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-      // Handle item selection
-      switch (item.getItemId()) {
-      case R.id.menuPause:
+    // Handle item selection
+    switch (item.getItemId()) {
+      case R.id.menuPauseResume:
         if (this.scheduler != null) {
           if (this.scheduler.isPauseRequested()) {
             this.scheduler.resume();
@@ -107,14 +102,15 @@ public class SpeedometerApp extends TabActivity {
         }
         return true;
       case R.id.menuQuit:
-        this.showDialog(this.EXIT_DIALOG_ID);
+        Log.i(TAG, "User requests exit. Quiting the app");
+        quitApp();
         return true;
       case R.id.menuSettings:
         // TODO(Wenjie): Add the settings page
-        return true;        
+        return true;
       default:
         return super.onOptionsItemSelected(item);
-      }
+    }
   }
     
   @Override
@@ -126,10 +122,10 @@ public class SpeedometerApp extends TabActivity {
      * However, it is known that the current Android OS does not take actions
      * on these properties but may enforce them in future versions.
      */ 
-    System.setProperty( "networkaddress.cache.ttl", "0");
-    System.setProperty( "networkaddress.cache.negative.ttl", "0");
-    Security.setProperty( "networkaddress.cache.ttl", "0");
-    Security.setProperty( "networkaddress.cache.negative.ttl", "0"); 
+    System.setProperty("networkaddress.cache.ttl", "0");
+    System.setProperty("networkaddress.cache.negative.ttl", "0");
+    Security.setProperty("networkaddress.cache.ttl", "0");
+    Security.setProperty("networkaddress.cache.negative.ttl", "0"); 
 
     Resources res = getResources(); // Resource object to get Drawables
     TabHost tabHost = getTabHost();  // The activity TabHost
@@ -152,7 +148,6 @@ public class SpeedometerApp extends TabActivity {
 
     tabHost.setCurrentTab(0);
     
-    createAlertDialog();
     RuntimeUtil.setActivity(this);
     
     // We only need one instance of scheduler thread
@@ -176,44 +171,12 @@ public class SpeedometerApp extends TabActivity {
       isBounded = false;
     }
   }
-   
-  /* TODO(Wenjie): This is a temporary solution to cleanly stop the app as well as all
-   * background threads whenever the BACK button is clicked. 
-   * Later should provide explicit buttons for user to stop the background threads and the app
-   * in a context menu.
-   * */
-  
-  @Override
-  protected Dialog onCreateDialog(int id, Bundle args) {
-    return this.exitConfirmationDialog;
-  }
   
   private void quitApp() {
     if (this.scheduler != null) {
       scheduler.requestStop();
     }
     this.finish();
-    System.gc();
     System.exit(0);
   }
-  
-  private void createAlertDialog() {
-    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    builder.setMessage("Do you want to exit Speedometer?")
-           .setCancelable(false)
-           .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-               @Override
-               public void onClick(DialogInterface dialog, int id) {
-                 Log.i(TAG, "User requests exit. Stopping all threads");
-                 quitApp();
-               }
-           })
-           .setNegativeButton("No", new DialogInterface.OnClickListener() {
-               @Override
-               public void onClick(DialogInterface dialog, int id) {
-                    dialog.cancel();
-               }
-           });
-    this.exitConfirmationDialog = builder.create();
-  } 
 }
