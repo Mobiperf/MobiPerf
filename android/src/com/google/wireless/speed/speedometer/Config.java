8b6fb16d6afbb2cabed403fb578cf44909b41ac0
wenjiezeng <wenjiezeng@google.com>
2011-08-09 16:03:41 -0700

    changed retry count from 10 to 3

--- android/src/com/google/wireless/speed/speedometer/Config.java
+++ android/src/com/google/wireless/speed/speedometer/Config.java
@@ -29,7 +29,7 @@ public interface Config {
   public static final long DEFAULT_CHECKIN_INTERVAL_SEC = 3 * 60L;
   public static final long MIN_CHECKIN_RETRY_INTERVAL_SEC = 10L;
   public static final long MAX_CHECKIN_RETRY_INTERVAL_SEC = 60L;
-  public static final int MAX_CHECKIN_RETRY_COUNT = 10;
+  public static final int MAX_CHECKIN_RETRY_COUNT = 3;
   public static final long PAUSE_BETWEEN_CHECKIN_CHANGE_SEC = 2L;
   // default minimum battery percentage to run measurements
   public static final int DEFAULT_BATTERY_THRESH_PRECENT = 60;
