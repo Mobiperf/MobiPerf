ecac427cf1b766d04fd956759fe1cd25d6fd6da3
wenjiezeng <wenjiezeng@google.com>
2011-08-29 14:44:33 -0700

    temp commit

--- android/src/com/google/wireless/speed/speedometer/MeasurementScheduler.java
+++ android/src/com/google/wireless/speed/speedometer/MeasurementScheduler.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
