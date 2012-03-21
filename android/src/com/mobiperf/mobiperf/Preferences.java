8283a65fc11b0a00cbd42eba7a40e9acab29b4ca
Junxian Huang <junxian.huang@gmail.com>
2012-03-21 19:20:29 -0400

    Removed options, changed pictures

--- android/src/com/mobiperf/mobiperf/Preferences.java
+++ android/src/com/mobiperf/mobiperf/Preferences.java
@@ -14,12 +14,6 @@
  */
 package com.mobiperf.mobiperf;
 
-import com.mobiperf.mobiperf.TimeSetting;
-import com.mobiperf.mobiperf.R;
-import com.mobiperf.speedometer.speed.Config;
-import com.mobiperf.speedometer.speed.Logger;
-import com.mobiperf.speedometer.speed.PeriodicTest;
-
 import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -30,27 +24,25 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.Toast;
+
+import com.mobiperf.speedometer.speed.Config;
+import com.mobiperf.speedometer.speed.Logger;
+import com.mobiperf.speedometer.speed.PeriodicTest;
 
 /*
  * Preference activity allowing user to enable/disable various settings.
  */
 public class Preferences extends PreferenceActivity {
 	public static final int NOTIFICATION_ID = 0;
-	private static boolean isNotificationEnabled = true; // Show notification by
-															// default
+	private static boolean isNotificationEnabled = true; // Show notification by default
 
-
-	//
 	// Define dialog ids
-
 	protected static final int DIALOG_PERIODIC = 0;
 	public static final int DIALOG_WARNING = 1;
 	protected static final int DIALOG_NOTIFICATION = 2;
@@ -65,50 +57,40 @@ public class Preferences extends PreferenceActivity {
 	public static final int PERIODIC_YES = 0;
 	public static final int PERIODIC_NO = 1;
 	final CharSequence[] periodicItems = { "Yes", "No" };
-	final CharSequence[] periodicPrompts = { "Periodic running is enabled",
-			"Periodical running is disabled" };
+	final CharSequence[] periodicPrompts = { "Periodic running is enabled", "Periodical running is disabled" };
 	public static final int NOTIFICATION_YES = 0;
 	public static final int NOTIFICATION_NO = 1;
 	final CharSequence[] notificationItems = { "Yes", "No" };
-	final CharSequence[] notificationPrompts = { "Notification is enabled",
-			"Notification is disabled" };
+	final CharSequence[] notificationPrompts = { "Notification is enabled", "Notification is disabled" };
 
-	
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.layout.preferences);
 		// Get the custom preference
 		Preference customPref = (Preference) findPreference("PeriodicPref");
-		customPref
-				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-					public boolean onPreferenceClick(Preference preference) {
-						SharedPreferences prefs = PreferenceManager
-								.getDefaultSharedPreferences(getBaseContext());
-						boolean CheckboxPreference = prefs.getBoolean(
-								"PeriodicPref", true);
-						if (CheckboxPreference == true) {
-							//by default, we run all tests periodically
-							enablePeriodicalRun(Preferences.this, "all"); 
-							Toast.makeText(getApplicationContext(),
-									"enabled periodic running",
-									Toast.LENGTH_SHORT).show();
-						}
-						if (CheckboxPreference == false) {
-							disablePeriodicalRun(Preferences.this);
-							Toast.makeText(getApplicationContext(),
-									"disable periodic running",
-									Toast.LENGTH_SHORT).show();
-						}
+		customPref.setOnPreferenceClickListener(new OnPreferenceClickListener(){
+			public boolean onPreferenceClick(Preference preference){
+				SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+				boolean CheckboxPreference = prefs.getBoolean("PeriodicPref", true);
+				if(CheckboxPreference == true){
+					enablePeriodicalRun(Preferences.this); 
+					Toast.makeText(getApplicationContext(), R.string.enablePeriodic, Toast.LENGTH_SHORT).show();
+				}
+				if(CheckboxPreference == false){
+					disablePeriodicalRun(Preferences.this);
+					Toast.makeText(getApplicationContext(), R.string.disablePeriodic, Toast.LENGTH_SHORT).show();
+				}
 
-						return true;
-					}
-				});
-		
+				return true;
+			}
+		});
+
 		Preference intervalPref = (Preference) findPreference("checkinIntervalPref");
 		Preference batteryPref = (Preference) findPreference("batteryMinThresPref");
 		Preference periodPref = (Preference) findPreference("periodtestPref");
-		
+
 		OnPreferenceChangeListener prefChangeListener = new OnPreferenceChangeListener() {
 			@Override
 			public boolean onPreferenceChange(Preference preference,
@@ -158,7 +140,7 @@ public class Preferences extends PreferenceActivity {
 				return true;
 			}
 		};
-		
+
 		intervalPref.setOnPreferenceChangeListener(prefChangeListener);
 		batteryPref.setOnPreferenceChangeListener(prefChangeListener);
 		periodPref.setOnPreferenceChangeListener(prefChangeListener);
@@ -179,23 +161,23 @@ public class Preferences extends PreferenceActivity {
 			builder.setTitle("Periodic MobiPerf currently running ...");
 			builder.setSingleChoiceItems(periodicItems,
 					isPeriodicalRunEnabled(this) ? 0 : 1,
-					new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog, int item) {
-							switch (item) {
-							case PERIODIC_YES:
-								//enablePeriodicalRun(Preferences.this);
-								break;
-							case PERIODIC_NO:
-								disablePeriodicalRun(Preferences.this);
-								break;
-							default:
-							}
-							Toast.makeText(getApplicationContext(),
-									periodicPrompts[item], Toast.LENGTH_SHORT)
-									.show();
-							Preferences.this.dismissDialog(DIALOG_PERIODIC);
-						}
-					});
+							new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int item) {
+					switch (item) {
+					case PERIODIC_YES:
+						//enablePeriodicalRun(Preferences.this);
+						break;
+					case PERIODIC_NO:
+						disablePeriodicalRun(Preferences.this);
+						break;
+					default:
+					}
+					Toast.makeText(getApplicationContext(),
+							periodicPrompts[item], Toast.LENGTH_SHORT)
+							.show();
+					Preferences.this.dismissDialog(DIALOG_PERIODIC);
+				}
+			});
 			dialog = builder.create();
 			break;
 		case DIALOG_NOTIFICATION:
@@ -203,40 +185,40 @@ public class Preferences extends PreferenceActivity {
 			builder.setTitle("Enable notification");
 			builder.setSingleChoiceItems(notificationItems,
 					isNotificationEnabled ? 0 : 1,
-					new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog, int item) {
-							switch (item) {
-							case NOTIFICATION_YES:
-								isNotificationEnabled = true;
-								if (isPeriodicalRunEnabled(Preferences.this))
-								break;
-							case NOTIFICATION_NO:
-								clearNotification(Preferences.this);
-								isNotificationEnabled = false;
-								break;
-							default:
-							}
-							Toast.makeText(getApplicationContext(),
-									notificationPrompts[item],
-									Toast.LENGTH_SHORT).show();
-							Preferences.this.dismissDialog(DIALOG_NOTIFICATION);
-						}
-					});
+							new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int item) {
+					switch (item) {
+					case NOTIFICATION_YES:
+						isNotificationEnabled = true;
+						if (isPeriodicalRunEnabled(Preferences.this))
+							break;
+					case NOTIFICATION_NO:
+						clearNotification(Preferences.this);
+						isNotificationEnabled = false;
+						break;
+					default:
+					}
+					Toast.makeText(getApplicationContext(),
+							notificationPrompts[item],
+							Toast.LENGTH_SHORT).show();
+					Preferences.this.dismissDialog(DIALOG_NOTIFICATION);
+				}
+			});
 			dialog = builder.create();
 			break;
 		case DIALOG_WARNING:
 			builder = new AlertDialog.Builder(this);
 			builder.setTitle("Notice")
-					.setMessage(WARNING)
-					.setCancelable(false)
-					.setPositiveButton("OK",
-							new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog,
-										int id) {
-									Preferences.this
-											.dismissDialog(DIALOG_WARNING);
-								}
-							});
+			.setMessage(WARNING)
+			.setCancelable(false)
+			.setPositiveButton("OK",
+					new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog,
+						int id) {
+					Preferences.this
+					.dismissDialog(DIALOG_WARNING);
+				}
+			});
 			dialog = builder.create();
 			break;
 		default:
@@ -263,62 +245,66 @@ public class Preferences extends PreferenceActivity {
 	}
 
 	/**
+	 * TODO
 	 * This is called when the "Periodic Running" is checked
 	 * @param context
 	 * @param periodtest
 	 */
-	public static void enablePeriodicalRun(Context context, String periodtest) {
+	public static void enablePeriodicalRun(Context context) {
 		
+		//TODO, get list of experiments from server and run them in the periodic
+		//We need to check for at the beginning of each cycle what test to run from server.
+		//by default, we run all tests periodically
 		Intent intent = new Intent(context, PeriodicTest.class);
-    	PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 
-    			Config.PERIODIC_REQUEST_CODE, intent, PendingIntent.FLAG_NO_CREATE);
-    	
-    	if(pendingIntent != null){
-    	}else{
-    		// Create new pending intent
-    		Bundle b = new Bundle();
-    		b.putString("test", periodtest);
-    		intent.putExtras(b);
-    		pendingIntent = PendingIntent.getBroadcast(context, Config.PERIODIC_REQUEST_CODE, intent, 0);
-    		AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-    		
-    		int interval = TimeSetting.getProgress()+10;
-    		
-    		//If less than 60, this is in minutes
-    		if (interval < 60) interval = interval * 1000 * 60;
-    		else {
-    			interval = interval - 59;
-    			interval = interval * 1000 * 3600;
-    		}
-			
-        	alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, 
-        			System.currentTimeMillis() + Config.PERIODIC_FIRST_RUN_STARTING_DELAY, 
-        			interval, pendingIntent);
-        	
-    	}
-	
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 
+				Config.PERIODIC_REQUEST_CODE, intent, PendingIntent.FLAG_NO_CREATE);
+
+		if(pendingIntent != null){
+		}else{
+			// Create new pending intent
+			Bundle b = new Bundle();
+			b.putString("test", "all");
+			intent.putExtras(b);
+			pendingIntent = PendingIntent.getBroadcast(context, Config.PERIODIC_REQUEST_CODE, intent, 0);
+			AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+
+			int interval = TimeSetting.getProgress()+10;
+
+			//If less than 60, this is in minutes
+			if (interval < 60) interval = interval * 1000 * 60;
+			else {
+				interval = interval - 59;
+				interval = interval * 1000 * 3600;
+			}
+
+			alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, 
+					System.currentTimeMillis() + Config.PERIODIC_FIRST_RUN_STARTING_DELAY, 
+					interval, pendingIntent);
+
+		}
+
 	}
 
 	public static void disablePeriodicalRun(Context context) {
 		Intent intent = new Intent(context, PeriodicTest.class);
-    	PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 
-    			Config.PERIODIC_REQUEST_CODE, intent, PendingIntent.FLAG_NO_CREATE);
-    	if(pendingIntent != null){
-    		AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-    		// Cancel alarm
-    		alarmManager.cancel(pendingIntent);
-    		// Remove the pending intent
-    		pendingIntent.cancel();
-    	}
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 
+				Config.PERIODIC_REQUEST_CODE, intent, PendingIntent.FLAG_NO_CREATE);
+		if(pendingIntent != null){
+			AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+			// Cancel alarm
+			alarmManager.cancel(pendingIntent);
+			// Remove the pending intent
+			pendingIntent.cancel();
+		}
 	}
 
 	public static boolean isPeriodicalRunEnabled(Context context) {
 		Intent intent = new Intent(context, PeriodicTest.class);
-    	PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 
-    			Config.PERIODIC_REQUEST_CODE, intent, PendingIntent.FLAG_NO_CREATE);
-    	if(pendingIntent == null)
-    		return false;
-    	return true;
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 
+				Config.PERIODIC_REQUEST_CODE, intent, PendingIntent.FLAG_NO_CREATE);
+		if(pendingIntent == null)
+			return false;
+		return true;
 	}
 
 	private static void clearNotification(Context context) {
