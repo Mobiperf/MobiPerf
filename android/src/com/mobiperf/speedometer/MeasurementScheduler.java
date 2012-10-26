ecb215281308558032deb7100794f0f54ff8e1f9
Dominic Hamon <dominich@google.com>
2012-10-26 13:43:52 -0700

    TEMPORARY: remove scheduled tasks

--- android/src/com/mobiperf/speedometer/MeasurementScheduler.java
+++ android/src/com/mobiperf/speedometer/MeasurementScheduler.java
@@ -639,8 +639,8 @@ public class MeasurementScheduler extends Service {
     removeAllUnscheduledTasks();
 
     for (MeasurementTask task : tasksFromServer) {
-      Logger.i("added task: " + task.toString());
-      this.submitTask(task);
+      //Logger.i("added task: " + task.toString());
+      //this.submitTask(task);
     }
   }
   
